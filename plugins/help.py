924ee8786e2d860b264c36cc1ec3c21e236e4078
Robin Mahieu <robin.mahieu@protonmail.com>
2021-10-10 11:40:46 +0200

    Add help command

---
+++ plugins/help.py
@@ -0,0 +1,33 @@
+import discourtesy
+
+from core.config import Config as config
+
+field_1 = (
+    "Brawl Box is a loot box simulator for Brawl Stars, designed for "
+    "entertainment purposes.\n\n"
+    "Please note that this content is not affiliated with, endorsed, "
+    "sponsored, or specifically approved by Supercell and Supercell is not "
+    "responsible for it. For more information see Supercell's Fan Content "
+    "Policy: https://supercell.com/fan-content-policy."
+)
+
+field_2 = (
+    "Discord: https://discord.gg/bXQaeFM\n"
+    "GitHub: https://github.com/nielsvv08/brawlbox\n"
+    "Patreon: https://patreon.com/brawlbox\n\n"
+    "Commands: **https://papier.dis.tf/brawlbox/commands**"
+)
+
+
+@discourtesy.command("help")
+async def help_command(application, _):
+    return discourtesy.utils.embed(
+        {
+            "title": f"This is Brawl Box version {application.version}!",
+            "color": config.colour,
+            "fields": [
+                {"name": "Brawl-who?", "value": field_1},
+                {"name": "Links", "value": field_2},
+            ],
+        }
+    )
