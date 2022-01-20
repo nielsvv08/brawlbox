77e28d7c10dc73ca60e90fddaa8ec2eb71fba190
Robin Mahieu <robin.mahieu@protonmail.com>
2022-01-20 15:05:00 +0100

    update invite link and remove title

--- plugins/invite.py
+++ plugins/invite.py
@@ -7,7 +7,6 @@ from core.config import Config as config
 async def invite_command(_, __):
     return discourtesy.utils.embed(
         {
-            "title": "Invite",
             "color": config.colour,
             "fields": [
                 {
@@ -16,7 +15,7 @@ async def invite_command(_, __):
                 },
                 {
                     "name": "Add the bot to another server.",
-                    "value": ">> https://discord.com/api/oauth2/authorize?client_id=531858459512012811&permissions=388176&scope=bot",
+                    "value": ">> https://discord.com/api/oauth2/authorize?client_id=531858459512012811&permissions=388176&scope=bot%20applications.commands",
                 },
             ],
         }
