bc5172a68264bda8b0616b25085871852e018b9b
Robin Mahieu <robin.mahieu@protonmail.com>
2022-02-05 15:01:58 +0100

    add plugins implicitly

--- run.py
+++ run.py
@@ -1,20 +1,9 @@
+import pathlib
 from core import Application
 
 application = Application()
 
-application.add_plugin("plugins.badges")
-application.add_plugin("plugins.beep")
-application.add_plugin("plugins.brawler")
-application.add_plugin("plugins.brawlers")
-application.add_plugin("plugins.claim")
-application.add_plugin("plugins.counter")
-application.add_plugin("plugins.eval")
-application.add_plugin("plugins.help")
-application.add_plugin("plugins.invite")
-application.add_plugin("plugins.leaderboard")
-application.add_plugin("plugins.levels")
-application.add_plugin("plugins.powerpoints")
-application.add_plugin("plugins.prestige")
-application.add_plugin("plugins.profile")
-application.add_plugin("plugins.version")
-application.add_plugin("plugins.vote")
+for path in pathlib.Path("plugins").glob("*.py"):
+    plugin = f"{path.parent}.{path.stem}"
+
+    application.add_plugin(plugin)
