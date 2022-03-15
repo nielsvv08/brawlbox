76ec44d81ea89a6d7b07c3e40816df268c96bacd
Robin Mahieu <robin.mahieu@protonmail.com>
2022-03-15 21:41:08 +0100

    add battle phrases

--- core/constants/battle.py
+++ core/constants/battle.py
@@ -12,13 +12,21 @@ class BattleConstants:
     ]
 
     win_phrases = [
+        # Papier
         "We are the champions!",
         "epic victory royale",
         "Obviously.",
+        "Never in doubt!"
+        # PiesPlazma
+        "GGWP :D",
+        "let's goooo",
     ]
 
     lose_phrases = [
+        # Papier
         "Better luck next time.",
         "We'll come back stronger.",
         "There's another chance tomorrow.",
+        # PiesPlazma
+        "Mission failed, we'll get 'em next time.",
     ]
