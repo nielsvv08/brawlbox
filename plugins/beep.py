18d5a2a0f3056149f74a20ff2d763e19c2526cd4
Robin Mahieu <robin.mahieu@protonmail.com>
2022-03-20 11:25:52 +0100

    update beep response

--- plugins/beep.py
+++ plugins/beep.py
@@ -3,4 +3,7 @@ import discourtesy
 
 @discourtesy.command("beep")
 async def beep_command(_, __):
-    return "boop"
+    return (
+        "Boop! Slash commands are enabled in this channel.\n"
+        "Want do disable them? Follow the guide at https://papier.dis.tf/brawlbox/guides/disable-slash-commands."
+    )
