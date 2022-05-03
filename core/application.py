9dbf50d03f5e3197a690245eadeaea6d776d6235
Robin Mahieu <robin.mahieu@protonmail.com>
2022-05-03 21:07:19 +0200

    bump version to 2.1.3

--- core/application.py
+++ core/application.py
@@ -23,7 +23,7 @@ class Application(discourtesy.Application):
 
         self.box_cooldown = dict()
 
-        self.version = "2.1.2"
+        self.version = "2.1.3"
 
         self.production = sys.platform == "linux"
 
