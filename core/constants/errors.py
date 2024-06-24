a57094807ab8cd6b9383e0cc90450f2fa7ffa7a8
Robin Mahieu <robin.mahieu@protonmail.com>
2024-06-24 12:57:55 +0200

    mention  command in profile not found errors

--- core/constants/errors.py
+++ core/constants/errors.py
@@ -1,3 +1,3 @@
 class ErrorsConstants:
-    profile_not_found = "The profile for **{}** could not be found."
-    profile_not_found_self = "Your profile could not be found."
+    profile_not_found = "The profile for **{}** could not be found. They should get started by using the `/box` slash command."
+    profile_not_found_self = "Your profile could not be found. You should get started by using the `/box` slash command."
